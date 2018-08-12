f96f4443fd0083109c060a0a8a21c1ae3b77d15c
xrobert <robert-x@mgdis.fr>
2018-08-13 01:00:07 +0200

    feat: update docker configuration

--- src/config/docker.js
+++ src/config/docker.js
@@ -1,5 +1,5 @@
 const config = {
-  MONGO_URL: process.env.MONGO_URL || "univ_auth_mongo",
+  MONGO_URL: process.env.MONGO_URL || "univ_mongo",
   MONGO_PORT: process.env.MONGO_PORT || "27017",
   LOG_LEVEL: process.env.LOG_LEVEL || "info"
 };
